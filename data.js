0770525fad2f0a80dae66d1050d4c4fa47ee925b
Eli Bales <elibales21@gmail.com>
2023-06-02 18:45:43 +0000

    feature 1 completed

--- data.js
+++ data.js
@@ -1,4 +1,9 @@
-const data = {
+window.addEventListener('beforeunload', event => {
+  const dataModel = JSON.stringify(data);
+  localStorage.setItem('userData', dataModel);
+});
+
+var data = {
   sunday: [],
   monday: [],
   tuesday: [],
@@ -9,3 +14,7 @@ const data = {
   editing: null,
   view: 'monday'
 };
+
+if (localStorage.getItem('userData') !== null) {
+  data = JSON.parse(localStorage.getItem('userData'));
+}
--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const $modalOverlay = document.querySelector('#modal-overlay');
 
 // Form elements
 const $entryForm = document.querySelector('#entry-form');
-const newTitle = $form.elements[0].value;
-const newUrl = $form.elements[1].value;
-const newNotes = $form.elements[2].value;
+// const newTitle = $form.elements[0].value;
+// const newUrl = $form.elements[1].value;
+// const newNotes = $form.elements[2].value;
 
 $addEntryButton.addEventListener('click', event => {
   $modalOverlay.classList.remove('hidden');
@@ -14,4 +14,14 @@ $addEntryButton.addEventListener('click', event => {
 $entryForm.addEventListener('submit', event => {
   event.preventDefault();
   $modalOverlay.classList.add('hidden');
+  const day = $entryForm.elements[0].value;
+  const time = $entryForm.elements[1].value;
+  const activity = $entryForm.elements[2].value;
+  const obj = {
+    entryDay: day,
+    entryTime: time,
+    entryActivity: activity
+  };
+  data[day].push(obj);
+  $entryForm.reset();
 });
